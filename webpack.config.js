e7c64917e7d83c3dc45dbef805ff84d65a197644
Astlantix <amzingfood@duck.com>
2024-12-06 11:16:47 -0800

    it's not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,28 @@
+const path = require('path');
+
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.(png|jpe?g|gif)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[path][name].[ext]',
-              },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
             },
-          ],
-        },
-      ],
-    },
-  };
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
